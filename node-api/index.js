a2317073b9a873b39c04b26d4de5969b127f0378
Pierre Kettmus <pkettmus@gmail.com>
2019-03-04 17:16:50 +0100

    update all

--- node-api/index.js
+++ node-api/index.js
@@ -19,7 +19,8 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cors());
 
-var urlMongo = "mongodb://admin:admin@annuel3al-shard-00-00-iui8c.mongodb.net:27017,annuel3al-shard-00-01-iui8c.mongodb.net:27017,annuel3al-shard-00-02-iui8c.mongodb.net:27017/pierre?ssl=true&replicaSet=annuel3AL-shard-0&authSource=admin&retryWrites=true";
+//var urlMongo = "mongodb://admin:admin@annuel3al-shard-00-00-iui8c.mongodb.net:27017,annuel3al-shard-00-01-iui8c.mongodb.net:27017,annuel3al-shard-00-02-iui8c.mongodb.net:27017/pierre?ssl=true&replicaSet=annuel3AL-shard-0&authSource=admin&retryWrites=true";
+var urlMongo = "mongodb://127.0.0.1:27017";
 
 //Connect to Mongoose and set connection variable
 mongoose.connect(urlMongo);
