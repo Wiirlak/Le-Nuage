d857a8e095e75fbf4ce1710eebac76b09da545db
Bastien <nisole.bastien@outlook.fr>
2019-05-03 20:53:46 +0200

    Added fully functionnal upload

--- Logiciel/src/sample/Fmxl/ControllerProfil.java
+++ Logiciel/src/sample/Fmxl/ControllerProfil.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -18,10 +20,18 @@ public class ControllerProfil {
     public static Stage stage;
 
     @FXML
-    public VBox profilPicture;
+    public VBox profil;
+
+    @FXML
+    public Circle profilPicture;
 
     public static void setStage(Stage stage) {
         ControllerProfil.stage = stage;
+    }
+
+    @FXML
+    public void initialize() {
+        profilPicture.setFill(new ImagePattern(new Image ("./sample/profile.jpg")));
     }
 
     @FXML
@@ -30,7 +40,7 @@ public class ControllerProfil {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choisir une nouvelle image");
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("All Images", "*.*","*.jpg", "*.png","*.gif"),
+                new FileChooser.ExtensionFilter("All Images","*.jpg", "*.png","*.gif"),
                 new FileChooser.ExtensionFilter("JPG", "*.jpg"),
                 new FileChooser.ExtensionFilter("PNG", "*.png"),
                 new FileChooser.ExtensionFilter("GIF", "*.gif")
@@ -38,10 +48,21 @@ public class ControllerProfil {
         File selected = fileChooser.showOpenDialog(stage);
         if(selected != null){
             System.out.println(selected.toURI().toString());
-            ImageView img = new ImageView();
-            img.setImage(new Image(selected.toURI().toString(),128,128,true,true));
-            profilPicture.getChildren().add(1,img);
+            profil.getChildren().remove(0);
+            //ImageView img = new ImageView();
+            //Image img = new Image(selected.toURI().toString(),128,128,true,true);
+           // profilPicture.setImage(img);
+            //profilPicture.getChildren().add(1,img);
+            Circle cir2 = new Circle(0,0,64);
+            Image img = new Image(selected.toURI().toString());
+            cir2.setFill(new ImagePattern(img));
+            profil.getChildren().add(0,cir2);
 
         }
+    }
+
+    @FXML
+    public void close(){
+        stage.close();
     }
 }
