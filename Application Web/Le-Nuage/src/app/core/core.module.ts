860629e40a0ac8713f5fc806c9ddd45e976fe87b
Alexis <vizuar.gamer@gmail.com>
2019-06-02 20:21:08 +0200

    updating to Angular 8 (and node 12.x)

--- Application Web/Le-Nuage/src/app/core/core.module.ts
+++ Application Web/Le-Nuage/src/app/core/core.module.ts
@@ -1,16 +1,20 @@
-import {NgModule} from '@angular/core';
-import { HomeComponent } from './components/home/home.component';
-import { WrongPathComponent } from './components/wrong-path/wrong-path.component';
+import {HomeComponent} from './components/home/home.component';
+import {WrongPathComponent} from './components/wrong-path/wrong-path.component';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent, WrongPathComponent, HeaderComponent, NavbarComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   exports: [
     HeaderComponent,
