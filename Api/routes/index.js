981772f34142bf78f2ca50ff444074a88ce031a6
Pierre Kettmus <pkettmus@gmail.com>
2019-06-17 19:52:09 +0200

    ajout download file entity, middleware for secure request

--- Api/routes/index.js
+++ Api/routes/index.js
@@ -3,9 +3,10 @@
 class RouterBuilder {
 
     build(app) {
+        app.use('/auth', require('./auth.router'));
+        app.use(require('../middleware').check());
         app.use('/user', require('./user.router'));
         app.use('/entity', require('./entity.router'));
-        app.use('/auth', require('./auth.router'));
         app.use('/nuage', require('./nuage.router'));
         app.use('/tree', require('./tree.router'));
     }
