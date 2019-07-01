f0d3f4785b4d988f79da49b8cfefcb26c9f4debd
Pierre Kettmus <pkettmus@gmail.com>
2019-07-01 19:56:22 +0200

    bug fix due to merge

--- Api/middleware.js
+++ Api/middleware.js
@@ -29,32 +29,4 @@ class CheckToken {
     }
 }
 
-
-const checkToken = (req, res, next) => {
-    if (req) {
-        const token = req.headers['x-access-token'];
-        if (!token) {
-            return res.status(401).send({auth: false, token: 'No token provided.'});
-        }
-
-        if (token) {
-            jwt.verify(token, process.env.SECRET, (err, decodes) => {
-                if (err) {
-                    return res.json({
-                        success: false,
-                        message: 'Token is not valid'
-                    });
-                } else {
-                    next();
-                }
-            });
-        } else {
-            return res.json({
-                success: false,
-                message: 'Auth token is not supplied'
-            });
-        }
-    }
-};
-
 module.exports = new CheckToken();
