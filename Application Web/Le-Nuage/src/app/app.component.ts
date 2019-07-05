7636b15acb4e1cdd81afebbf0e4c0fc140118cbc
Alexis <vizuar.gamer@gmail.com>
2019-07-05 18:48:38 +0200

    hide when no log

--- Application Web/Le-Nuage/src/app/app.component.ts
+++ Application Web/Le-Nuage/src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthentificationService} from './admin/services/authentification/authentification.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private authentificationService: AuthentificationService) {}
+export class AppComponent implements OnInit {
+  displayNavBar: boolean;
+
+  constructor(private authentificationService: AuthentificationService,
+              private router: Router) {}
   title = 'Le-Nuage';
 
   isLogged() {
     return this.authentificationService.loggingIn('jean@toto.fr', 'testtest');
+  }
+
+  ngOnInit(): void {
+    this.displayNavBar = (this.router.url !== '/');
   }
 }
