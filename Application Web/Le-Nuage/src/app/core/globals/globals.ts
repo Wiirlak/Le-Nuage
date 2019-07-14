9c1c7a08a453a966523591d284d472f2e723918f
Desjardins Alexis <vizuar.gamer@gmail.com>
2019-07-14 21:18:33 +0200

    last upload. END

--- Application Web/Le-Nuage/src/app/core/globals/globals.ts
+++ Application Web/Le-Nuage/src/app/core/globals/globals.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  apiPath = 'http://localhost:3000/';
+  apiPath = 'http://lenuage.francecentral.cloudapp.azure.com/api/';
+  // apiPath = 'http://localhost:3000/';
 }
